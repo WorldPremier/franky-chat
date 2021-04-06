86bb95d5dfa63c4301e9b23db4071e237c357c0a
Franklyn Reyes <franklynreyes777@yahoo.com>
2021-04-06 17:19:11 -0400

    myfirstcommit

--- src/index.js
+++ src/index.js
@@ -11,7 +11,14 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// const functions = require('firebase-functions');
+// const Filter =require('bad-words');
+
+// const admin = require('firebase-admin');
+// admin.initializeApp();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// const db =admin.firestore();
+// exports.detectEvilUsers = functions.firestore
 reportWebVitals();
